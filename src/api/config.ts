6bde705f3822cb967cfb65e7af46486f59ef260f
r.kowdamapalli <r.kowdampalli@accenture.com>
2025-07-17 09:04:22 +0200

    fix config

--- src/api/config.ts
+++ src/api/config.ts
@@ -3,7 +3,7 @@
 
 // The API base URL from environment variables
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 
-  'https://fuelsync-api-demo-bvadbhg8bdbmg0ff.germanywestcentral-01.azurewebsites.net';
+  'https://fuelsync-cjerbvabbsf8bsgu.eastasia-01.azurewebsites.net';
 
 // The API version
 export const API_VERSION = 'v1';
